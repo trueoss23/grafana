864e101ac296b8fa753a88688e3e2640fa828036
Igor <trueoss1992@gmail.com>
2023-08-08 22:14:34 +0700

    create app

---
+++ app.py
@@ -0,0 +1,17 @@
+from loguru import logger
+import time
+
+logger.add("app.log", rotation="10 MB", level="INFO")
+
+
+def app():
+    for _ in range(100):
+        logger.info("Это информационное сообщение")
+        logger.warning("Это предупреждающее сообщение")
+        logger.error("Это сообщение об ошибке")
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    app()
+
